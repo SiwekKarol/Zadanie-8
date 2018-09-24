40f5caaa794ce7f6700f2414002c3102415ab0cc
KarolSiwek <siwek.karol1992@gmail.com>
2018-09-24 21:58:47 +0200

    Zadanie z trzema imionami w tablicy.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    String[] names = new String[3];
+	    names[0] = "Janusz";
+	    names[1] = "Grażyna";
+	    names[2] = "Sebastian";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+    }
+}
